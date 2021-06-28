11a61a7082af814dcbec2bc267161f8d3a6d079b
“Valber” <valber.lrenan@mercadolivre.com>
2021-06-28 19:09:33 -0300

    correção dependencia e classe myController

--- src/main/java/com/valber/renan/injecaodependencia/InjecaoDependenciaApplication.java
+++ src/main/java/com/valber/renan/injecaodependencia/InjecaoDependenciaApplication.java
@@ -1,13 +1,21 @@
 package com.valber.renan.injecaodependencia;
 
+import com.valber.renan.injecaodependencia.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class InjecaoDependenciaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InjecaoDependenciaApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(InjecaoDependenciaApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
--- src/test/java/com/valber/renan/injecaodependencia/InjecaoDependenciaApplicationTests.java
+++ src/test/java/com/valber/renan/injecaodependencia/InjecaoDependenciaApplicationTests.java
@@ -3,7 +3,6 @@ package com.valber.renan.injecaodependencia;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class InjecaoDependenciaApplicationTests {
 
 	@Test
